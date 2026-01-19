aa1a2d983b0d28f43465d3d73129c9eb42e1b6f2
FaraizAliMehmood <ibrahimsoft004@gmail.com>
2026-01-19 09:12:33 +0500

    Update firebase.ts
    save

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -4,13 +4,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBf-RCfcMUt4JhzyUo5Q2icWHvwUHgd3TQ",
-  authDomain: "shopsand-3508c.firebaseapp.com",
-  projectId: "shopsand-3508c",
-  storageBucket: "shopsand-3508c.firebasestorage.app",
-  messagingSenderId: "135163729725",
-  appId: "1:135163729725:web:ff314cf0cbaf802864d754",
-  measurementId: "G-KH4NC2N9TP"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "P"
 };
 
 // Initialize Firebase
